0efeaa088f44a72d1484a3f0c4dbe81a9572d8a5
Joel <da1nerd@pm.me>
2021-05-04 17:29:24 +0700

    updated storybook

--- stories/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Button} from '@storybook/react/demo';
-
-/**
- * Develop your components in isolation here using https://storybook.js.org/
- */
-
-export const Primary = () => <Button primary><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>;
